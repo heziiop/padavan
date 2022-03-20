34f380d4debdd6988e80885dc751435343683676
hwlon <2849273+hwlon@users.noreply.github.com>
2022-03-20 15:10:04 +0800

    Update variables.c

--- trunk/user/httpd/variables.c
+++ trunk/user/httpd/variables.c
@@ -1014,11 +1014,14 @@
 #if defined(APP_WIREGUARD)
 	struct variable variables_WIREGUARD[] = {
 			{"wireguard_enable", "", NULL, EVM_RESTART_WIREGUARD},
-			{"wireguard_localip", "", NULL, EVM_RESTART_WIREGUARD},
 			{"wireguard_localkey", "", NULL, EVM_RESTART_WIREGUARD},
+			{"wireguard_listenport", "", NULL, EVM_RESTART_WIREGUARD},
+			{"wireguard_localip", "", NULL, EVM_RESTART_WIREGUARD},		
 			{"wireguard_peerkey", "", NULL, EVM_RESTART_WIREGUARD},
+			{"wireguard_presharedkey", "", NULL, EVM_RESTART_WIREGUARD},
+			{"wireguard_allowedips", "", NULL, EVM_RESTART_WIREGUARD},
 			{"wireguard_peerip", "", NULL, EVM_RESTART_WIREGUARD},
-			{"wireguard_localip", "", NULL, EVM_RESTART_WIREGUARD},
+			{"wireguard_keepalive", "", NULL, EVM_RESTART_WIREGUARD},
 			{0,0,0,0}
 		};
 #endif
